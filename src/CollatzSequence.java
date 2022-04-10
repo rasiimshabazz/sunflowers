8e9ec45d5a7f1eb1b44b1601bb1dfbdca6ab9510
russimsha <drewdocode@gmail.com>
2022-04-10 17:23:04 -0400

    replace value object w map

--- src/CollatzSequence.java
+++ src/CollatzSequence.java
@@ -1,39 +1,36 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class CollatzSequence {
 
     public static long getNumberWithMostSteps(List<Integer> numbers) {
-        List<StepCount> stepCounts = new ArrayList<>();
+        HashMap<Long, Long> numberAndCount = new HashMap<>();
 
         numbers.stream().forEach(number -> {
 
             long input = !((long) number <= 0 || (long) number > 1000000) ? (long) number : 0;
-            int count = 0;
+            long count = 0;
             while (input > 1) {
                 if (input % 2 == 0) input = input / 2;
                 else input = (3 * input) + 1;
                 count++;
             }
-            stepCounts.add(new StepCount(number, count));
+            numberAndCount.put(Long.valueOf(number), count);
+
         });
 
-        StepCount stepCount = stepCounts.stream()
-                .max(Comparator.comparing(stepCount1 -> stepCount1.stepCount))
-                .orElseThrow(NoSuchElementException::new);
-        return stepCount.number;
+        return maxUsingIteration(numberAndCount);
     }
 
-    static class StepCount {
-        private long number;
-        private long stepCount;
-
-        public StepCount(long number, long stepCount) {
-            this.number = number;
-            this.stepCount = stepCount;
+    public static <K, V extends Comparable<V>> K maxUsingIteration(Map<K, V> map) {
+        Map.Entry<K, V> maxEntry = null;
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (maxEntry == null || entry.getValue()
+                    .compareTo(maxEntry.getValue()) > 0) {
+                maxEntry = entry;
+            }
         }
+        return maxEntry.getKey();
     }
+
 
 }
