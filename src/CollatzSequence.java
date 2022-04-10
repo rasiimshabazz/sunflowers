a6e34c9d3bdba7b02c3a58e00bdea80987d20093
russimsha <drewdocode@gmail.com>
2022-04-10 15:20:46 -0400

    inline more

--- src/CollatzSequence.java
+++ src/CollatzSequence.java
@@ -13,20 +13,21 @@ public class CollatzSequence {
         List<StepCount> stepCounts = new ArrayList<>();
 
         numbers.stream().forEach(number -> {
-            final int COLLATZ_LIMIT = 1000000;
 
-            long input = !((long) number <= 0 || (long) number > COLLATZ_LIMIT) ? (long) number : 0;
+            long input = !((long) number <= 0 || (long) number > 1000000) ? (long) number : 0;
             List<Step> steps1 = new ArrayList<>();
             while (input > 1) {
                 Step step;
-                if (input % 2 == 0) step = new EvenStep(input);
-                else step = new OddStep(input);
+                if (input % 2 == 0) {
+                    step = new EvenStep(input);
+                }
+                else {
+                    step = new OddStep(input);
+                }
                 steps1.add(step);
                 input = step.run();
             }
-            List<Step> steps = steps1;
-            long sequenceCount = steps.size();
-            stepCounts.add(new StepCount(number, sequenceCount));
+            stepCounts.add(new StepCount(number, steps1.size()));
         });
 
         StepCount stepCount = stepCounts.stream()
