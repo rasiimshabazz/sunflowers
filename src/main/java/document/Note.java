2619a7890eee169741693bc7bcdf45bc9966fb13
russimsha <drewdocode@gmail.com>
2022-04-22 15:34:49 -0400

    isformable

--- src/main/java/document/Note.java
+++ src/main/java/document/Note.java
@@ -4,7 +4,7 @@ class Note {
 
     private String text;
 
-    public Note(String text) {
+    Note(String text) {
         if (text == null) text = "";
         this.text = text;
     }
@@ -12,8 +12,26 @@ class Note {
     boolean isFormableFrom(String document) {
         if ((text.isEmpty()) && (document.isEmpty() || document == null)) return true;
         if ((text.isEmpty()) || (document.isEmpty() || document == null)) return false;
-
+        for (int i = 0; i < this.text.length(); i++) {
+            char character = this.text.charAt(i);
+            if (documentContainsCharacter(document, character)) {
+                document = removeCharacterFromDocument(document, character);
+            }
+            else {
+                System.out.println(document);
+                return false;
+            }
+        }
         return true;
+    }
+
+    private String removeCharacterFromDocument(String document, char character) {
+        document = document.replaceFirst("" + document.charAt(document.indexOf(character)), "" + Character.MIN_VALUE);
+        return document;
+    }
+
+    private boolean documentContainsCharacter(String document, char character) {
+        return document.indexOf(character) >= 0;
     }
 
 }
--- src/test/java/document/NoteTest.java
+++ src/test/java/document/NoteTest.java
@@ -8,10 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class NoteTest {
 
     @Test
-    void isFormable() {
+    void isFormable_guards() {
         assertTrue(new Note("").isFormableFrom(""));
         assertFalse(new Note("any text").isFormableFrom(""));
         assertFalse(new Note("").isFormableFrom("any document"));
+    }
+
+    @Test
+    void isFormable() {
+        assertTrue(new Note("scary note").isFormableFrom("a bunch of text from today's newspaper"));
+        assertTrue(new Note("abc").isFormableFrom("ann bob cat"));
+        assertTrue(new Note("abc abc").isFormableFrom("cann bob cat"));
+    }
+
+    @Test
+    void isFormable_false() {
+        assertFalse(new Note("even scarier note").isFormableFrom("a bunch of text from today's newspaper"));
+        assertFalse(new Note("abc abc").isFormableFrom("abc "));
+        assertFalse(new Note("abc abc").isFormableFrom("ann bob cat"));
     }
 
 }
